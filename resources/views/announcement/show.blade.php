155f20210f1ef8391fb4e34f90ebe35215a23ddc
Valentina Rappazzo <valentinarappazzo90@gmail.com>
2021-03-18 19:07:43 +0100

    ritocchi front end

--- resources/views/announcement/show.blade.php
+++ resources/views/announcement/show.blade.php
@@ -4,21 +4,19 @@
     <div class="row mt-5">
         <div class="col-12 mt-5">
             <h1 class="text-center">Ecco l'annuncio</h1>
-
         </div>
-
     </div>
-    <div class="row mt-5">
-        <div class="col-md-4">
-            <img src="https://picsum.photos/300/150" alt="">
 
+    <div class="row mt-5  justify-content-center ">
+        <div class="col-12 col-lg-8 mt-2">
+            <img src="https://picsum.photos/300/150" alt="">
         </div>
-        <div class="col-md-8">
+        <div class="col-12 col-lg-4 text-center mt-2 ">
             <h3>Titolo: </h3><span>{{$announcement->title}}</span>
             <h3>Descrizione: </h3><span>{{$announcement->body}}</span>
             <h3>Prezzo: </h3><span>{{$announcement->price}}</span>
-
         </div>
+
     </div>
 </div>
 </x-layouts>
